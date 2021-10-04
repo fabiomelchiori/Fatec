150ea7428f6dfa37d32875646df959d69cc1665f
fabiomelchiori <fabio.collaboni@gmail.com>
2021-10-03 21:45:48 -0300

    ProvaFinal

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button,
+  FlatList,
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View 
+} from 'react-native';
 
-export default function App() {
+export default function EntradaDeDados() {
+  const [descricao, setDescricao] = useState ('')
+  const [fabricante, setFabricante] = useState('')
+  const [ExibeItens, setItem] = useState([])
+  const [contador, setContador] = useState(0)
+
+  const capturarDescricao = (descricao) => {
+    setDescricao(descricao)
+  }
+
+  const capturarFabricante = (fabricante) => {
+    setFabricante(fabricante)
+  }
+
+  const adicionarItem = () => {
+    setItem (ExibeItens => {
+      setContador (contador + 1)
+      let aux = [{key: contador.toString(), value: "Descrição: " + descricao + '\n' + "Fabricante: " + fabricante}, ...ExibeItens]
+      setDescricao('')
+      setFabricante('')
+      console.log (aux)
+      return aux
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={styles.telaPrincipalView}>
+      <View>
+        <TextInput 
+          placeholder="Descrição do Item"
+          style={styles.itemTextInput}
+          onChangeText={capturarDescricao}
+          value={descricao}
+        />
+        <TextInput 
+          placeholder="Fabricante do Item"
+          style={styles.itemTextInput}
+          onChangeText={capturarFabricante}
+          value={fabricante}
+        />
+        <Button 
+          title="Cadastrar"
+          onPress={adicionarItem}
+        />
+      </View>
+      <View
+        style={{marginTop: 40}}>
+        <FlatList 
+          data={ExibeItens}
+          renderItem={(ExibeItens) => (
+            <View 
+              style={styles.itemNaLista}>
+              <Text>{ExibeItens.item.value}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  telaPrincipalView: {
+    padding: 100
   },
+  itemTextInput :{
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    textAlign: 'center'
+  },
+  itemNaLista: {
+    padding: 12,
+    backgroundColor: '#CCE',
+    borderColor: '#CCC',
+    marginBottom: 8,
+    borderRadius: 8,
+    textAlign: 'center',
+  }
 });
